678c44c3f05b32d04296c027b7b36fa3684d6c16
Curtis Ullerich <curtisullerich@gmail.com>
2013-11-04 23:16:09 -0600

    added metro and multi-channel to etude01

--- impromptu/etude01.scm
+++ impromptu/etude01.scm
@@ -5,7 +5,7 @@
 ;(define piano (au:make-node "aumu" "dls " "appl"))
 ;(define piano (au:make-node "aumu" "NiMa" "-NI-"))
 (define piano (au:make-node "aumu" "NiO5" "-NI-"))
-(au:connect-node piano 0 *au:output-node* 0)
+(au:connect-node piano 1 *au:output-node* 0)
 (au:update-graph)
 (au:open-view piano)
 (define select-random
@@ -13,39 +13,62 @@
     (let ((len (length ls)))
       (list-ref ls (random len)))))
 
+(define ostinato 
+   (lambda (time note duration)
+      (play-note (*metro* time) piano (+ 40 note) 120 (*metro* 'dur duration) 0)
+      (callback 
+        (*metro* (+ time (* .5 duration))) 
+        'ostinato 
+          (+ time duration) 
+          (random (cdr (assoc note '((0 5 7)
+                                     (7 0 5)
+                                     (5 0)
+                                    ))))
+          duration
+      )
+   )
+)
+   
+(ostinato (*metro* 'get-beat) 0 4.0)
+   
 (define loop 
-  (lambda (time index pan velocity)
-    (define pitches '(0 4 7 9 11 12 16 19 21 23 24))
+  (lambda (time index pan velocity duration)
+    (define pitches '(0 5 7 9 11 12 16 19 21 23 24))
     ;(define pitches '(0 5 7 8 11 14 16 19 22))
     ;(define pitches '(0))
-    (au:midi-out time piano *io:midi-cc* 0 10 pan)
+    ;(au:midi-out time piano *io:midi-cc* 0 10 pan)
     ;(au:midi-out time piano *io:midi-cc* 0 10 (random 0 127))
     ;(au:midi-out time piano *io:midi-cc* 0 10 0)
     (let 
       (
-        (newpan (max 1 (min 127 (+ pan (select-random '(-10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 10))))))
-        (newindex (max 0 (min (- (length pitches) 1) (+ index (select-random '(-2 -1 1 2) )))))
-        ;(newvelocity (max 50 (min 100 (+ velocity (select-random '(-5 0 5))))))
-        (newvelocity 80)
+        ;(newpan (max 1 (min 127 (+ pan (select-random '(-10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 10))))))
+        (newindex (max 0 (min (- (length pitches) 1) (+ index (select-random '(-1 1) )))))
+        (newvelocity (max 50 (min 50 (+ velocity (select-random '(-5 0 5))))))
+        ;(newvelocity 80)
+        ;(newindex 0)
+        (newpan 50)
       )
       (print newindex)
-      (play-note time piano 
+      (play-note (*metro* time) piano 
         (+ 
           (list-ref pitches newindex)
           40
         )
-        velocity 
-        10000
+        newvelocity 
+        (*metro* 'dur duration)
+        1
       )
    ;(au:midi-out (+ time (/ *second* 8notest)) piano *io:midi-cc* 0 10 127)
-   (if (eqv? (random 0 2) 0)
+   ;(if (eqv? (random 0 2) 0)
      ;(play-note (+ time (/ *second* 8)) piano (+ 50 (select-random pitches)) 80 10000)
-   )
-   (callback (+ time (/ *second* 8)) 'loop (+ time (/ *second* 8)) newindex newpan newvelocity)        
+   ;)
+   (callback (*metro* (+ time (* .5 duration))) 'loop (+ time duration) newindex newpan newvelocity duration)        
    );end of let
   )
 )
-(loop (now) 0 50 80)
+(loop (*metro* 'get-beat) 0 50 80 .25)
+
+
 
 
 
@@ -61,7 +84,7 @@
       (callback (*metro* (+ time (* .5 duration))) 'drum-loop (+ time duration)
                 (random (list 0.5)))))
  
-(drum-loop (*metro* 'get-beat) 0.5)
+(drum-loop (*metro* 'get-beat) 0.25)
 
 
 
